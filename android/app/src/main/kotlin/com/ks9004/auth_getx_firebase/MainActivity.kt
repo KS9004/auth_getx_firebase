852b6f243d25a16b0ad109b413d79fe9dd47d912
KS9004 <uic.19mca8068@gmail.com>
2021-01-26 11:16:23 +0530

    Intial commit

---
+++ android/app/src/main/kotlin/com/ks9004/auth_getx_firebase/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ks9004.auth_getx_firebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/view/sign_up.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SignUp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+      body: SafeArea(child: Container(
+
+      )),
+
+    );
+  }
+}
