985111f1e64a64f30a3673f6ecbc9f48c88397c1
KS9004 <uic.19mca8068@gmail.com>
2021-01-27 12:09:18 +0530

    fixed bottom overflowed while keyboard appears

--- lib/view/sign_up.dart
+++ lib/view/sign_up.dart
@@ -7,103 +7,133 @@ class SignUp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     backgroundColor: Colors.pinkAccent,
+      backgroundColor: Colors.pinkAccent,
       body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
-            Container(
-              height: 550,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(40.0),topRight: Radius.circular(40.0))
-              ),
-              child: Padding(
-                padding: EdgeInsets.only(top: 30.0,left: 20.0,right: 20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                   Text('Sign Up',style: GoogleFonts.aBeeZee(textStyle: TextStyle(
-                       color: Color(0xff003459),
-                       fontSize: 60.0,
-                       fontWeight: FontWeight.bold
-                   ),)
-                   ),
-                    SizedBox(height: 50.0,),
-
-                    Text("Email",style: GoogleFonts.exo2(
-                      textStyle: TextStyle(
-                          fontSize: 25.0,
-                          fontWeight: FontWeight.bold
-                      ),
-                    ),
-                    ),
-                    SizedBox(height: 5.0,),
-                    TextFormField(
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                        fillColor: Color(0xffFEF3F1),
-                        filled: true,
-                        hintStyle: GoogleFonts.exo2(
-                          textStyle: TextStyle(color:Color(0xffFC7D90)),
-                        ),
-                        prefixIcon: Icon(Icons.email_rounded,color:Color(0xffFC7D90),),
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide.none,
-                          borderRadius: BorderRadius.circular(15.0),
-                        ),
-                        hintText: 'Enter Email Here',
-                      ),
-                      autofocus: false,
-                    ),
-                    SizedBox(height: 20.0,),
-                    Text("Password",style: GoogleFonts.exo2(
-                      textStyle: TextStyle(
-                          fontSize: 25.0,
-                          fontWeight: FontWeight.bold
-                      ),
-                    ),
-                    ),
-                    SizedBox(height: 5.0,),
-                    TextFormField(
-                      keyboardType: TextInputType.visiblePassword,
-                      decoration: InputDecoration(
-                        fillColor: Color(0xffFEF3F1),
-                        filled: true,
-                        hintStyle: GoogleFonts.exo2(
-                          textStyle: TextStyle(color:Color(0xffFC7D90)),
-                        ),
-                        prefixIcon: Icon(Icons.lock_rounded,color:Color(0xffFC7D90)),
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide.none,
-                          borderRadius: BorderRadius.circular(15.0),
-                        ),
-                        hintText: 'Enter Password Here',
-                      ),
-                      autofocus: false,
-                      obscureText: true,
-                    ),
-                    SizedBox(height:50.0,),
-                    Container(
-                      width: double.infinity,
-                      height: 60.0,
-                      decoration: BoxDecoration(
-                          color: Colors.pinkAccent,
-                        borderRadius: BorderRadius.circular(15.0)
-                      ),
-                      child: MaterialButton(
-                        onPressed: null,
-                        child: Text("Next",style:GoogleFonts.aBeeZee(
-                         textStyle: TextStyle(
-                              color: Colors.white,
-                              fontSize: 40.0
+            SizedBox(height: 250,), // Image.sasset('assets/sign_up2.svg',width: 400,),
+            Expanded(
+              child: Container(
+                height: 600,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(40.0),topRight: Radius.circular(40.0))
+                ),
+                child: Padding(
+                  padding: EdgeInsets.only(top: 30.0,left: 20.0,right: 20.0),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                       Text('Sign Up',style: GoogleFonts.aBeeZee(textStyle: TextStyle(
+                           color: Color(0xff003459),
+                           fontSize: 60.0,
+                           fontWeight: FontWeight.bold
+                       ),)
+                       ),
+                        SizedBox(height: 45.0,),
+                        Text("Email",style: GoogleFonts.exo2(
+                          textStyle: TextStyle(
+                              fontSize: 25.0,
+                              fontWeight: FontWeight.bold
                           ),
                         ),
                         ),
-                      ),
+                        SizedBox(height: 5.0,),
+                        TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                            fillColor: Color(0xffFEF3F1),
+                            filled: true,
+                            hintStyle: GoogleFonts.exo2(
+                              textStyle: TextStyle(color:Color(0xffFC7D90)),
+                            ),
+                            prefixIcon: Icon(Icons.email_rounded,color:Color(0xffFC7D90),),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(15.0),
+                            ),
+                            hintText: 'Enter Email Here',
+                          ),
+                          autofocus: false,
+                        ),
+                        SizedBox(height: 20.0,),
+                        Text("Password",style: GoogleFonts.exo2(
+                          textStyle: TextStyle(
+                              fontSize: 25.0,
+                              fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        ),
+                        SizedBox(height: 5.0,),
+                        TextFormField(
+                          keyboardType: TextInputType.visiblePassword,
+                          decoration: InputDecoration(
+                            fillColor: Color(0xffFEF3F1),
+                            filled: true,
+                            hintStyle: GoogleFonts.exo2(
+                              textStyle: TextStyle(color:Color(0xffFC7D90)),
+                            ),
+                            prefixIcon: Icon(Icons.lock_rounded,color:Color(0xffFC7D90)),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(15.0),
+                            ),
+                            hintText: 'Enter Password Here',
+                          ),
+                          autofocus: false,
+                          obscureText: true,
+                        ),
+                        SizedBox(height: 20.0,),
+                        Text("Phone Number",style: GoogleFonts.exo2(
+                          textStyle: TextStyle(
+                              fontSize: 25.0,
+                              fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        ),
+                        SizedBox(height: 5.0,),
+                        TextFormField(
+                          keyboardType: TextInputType.phone,
+                          decoration: InputDecoration(
+                            fillColor: Color(0xffFEF3F1),
+                            filled: true,
+                            hintStyle: GoogleFonts.exo2(
+                              textStyle: TextStyle(color:Color(0xffFC7D90)),
+                            ),
+                            prefixIcon: Icon(Icons.lock_rounded,color:Color(0xffFC7D90)),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(15.0),
+                            ),
+                            hintText: 'Enter Phone Number Here',
+                          ),
+                          autofocus: false,
+                        ),
+                        SizedBox(height:50.0,),
+                        Container(
+                          width: double.infinity,
+                          height: 60.0,
+                          decoration: BoxDecoration(
+                              color: Colors.pinkAccent,
+                            borderRadius: BorderRadius.circular(15.0)
+                          ),
+                          child: MaterialButton(
+                            onPressed: null,
+                            child: Text("Next",style:GoogleFonts.aBeeZee(
+                             textStyle: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 40.0
+                              ),
+                            ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
               ),
             )
